22c04f6293d35954929280a29d792eb7e54d0e1f
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-07-28 09:42:35 -0600

    Build a new model coefficients table

--- R/plots_tables.R
+++ R/plots_tables.R
@@ -9,3 +9,23 @@ make_map <- function(links, projects){
   
   
 }
+
+
+make_coefficient_table <- function(mc_constants, mc_coefficients, dc_coefficients){
+  
+  # mode choice coefficients
+  mc <- bind_rows(
+    mc_constants,
+    mc_coefficients
+  ) %>%
+    mutate(model = "Mode Choice")
+  
+  
+  # destination choice coefficients
+  dc <- dc_coefficients %>%
+    mutate(model = "Destination Choice")
+  
+  bind_rows(dc, mc) %>%
+    select(model, var, HBW, HBO, NHB)
+  
+}
