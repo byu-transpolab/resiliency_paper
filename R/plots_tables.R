38a3b9067b042f7b4940fe4357ff069e09e26def
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-07-28 08:17:49 -0600

    Start using targets to make and build tables and images
    Former-commit-id: 3e65cc8ab002ca86aa00f9207232ef69260ced18

---
+++ R/plots_tables.R
@@ -0,0 +1,11 @@
+#' Make a plot of the links
+#' 
+#' @param links An sf object with highway links and project IDs
+#' @param projects A tibble with project data
+#' 
+#' @return A map
+#' 
+make_map <- function(links, projects){
+  
+  
+}
