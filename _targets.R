3a165d59df3e59aa1ccb555b7bba0b5dab4dc847
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-07-28 20:27:37 -0600

    Build project costs up to link maps
    Former-commit-id: d2b0ff86e89ab2c7a5894ed91d444d3572fedd3b

--- _targets.R
+++ _targets.R
@@ -10,16 +10,35 @@ library(targets)
 # This is where you write source(\"R/functions.R\")
 # if you keep your functions in external scripts.
 source("R/plots_tables.R")
+source("R/project_calculator.R")
+
+# mode choice cost coefficient in c/mile
+mc_cost_coef <- -0.0016
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("tidyverse", "sf", "readxl"))
+tar_option_set(packages = c("tidyverse", "sf", "readxl", "leaflet"))
 
 # End this file with a list of target objects.
 list(
-  tar_target(links, st_read("data/Risks Links Shapefile/risk_links_network.shp")),
-  tar_target(projects, read_xlsx("images/logsumrank.xlsx")),
-  tar_target(map, make_map(links, projects)),
+  # links with project id
+  tar_target(links_file, "data/links.geojson", format = "file"),
+  tar_target(links, st_read(links_file)),
   
+  # project costs
+  tar_target(prod_rds, "data/productions.rds", format = "file"),
+  tar_target(prod, read_rds(prod_rds)),
+  tar_target(lsum_rds, "data/logsums.rds", format = "file"),
+  tar_target(logsums, read_rds(lsum_rds)),
+  
+  tar_target(deltas, calculate_deltas(prod, logsums)),
+  tar_target(costs, calculate_costs(deltas, mc_cost_coef)),
+  
+  
+  # project map
+  tar_target(mapdata, make_mapdata(links, costs)),
+  
+  
+  # utility coefficients
   tar_target(mc_constants_file, "data/MC_Constants.csv", format = "file"),
   tar_target(mc_coefficients_file, "data/MC_Coefficients.csv", format = "file"),
   tar_target(dc_coefficients_file, "data/DC_Parameters.csv", format = "file"),
